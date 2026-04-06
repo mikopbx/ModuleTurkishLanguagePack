c04e296fa27b7f0954b4277110364478d7ee01fd
Nikolay Beketov <nbek@miko.ru>
2026-04-06 14:12:51 +0700

    Initial commit: ModuleTurkishLanguagePack language pack for MikoPBX

---
+++ Messages/en/ModuleTurkishLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModuleTurkishLanguagePack' => 'Turkish Language Pack',
+    'SubHeaderModuleTurkishLanguagePack' => 'Complete Turkish language support for MikoPBX',
+];
---
+++ Messages/ru/ModuleTurkishLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModuleTurkishLanguagePack' => 'Языковой пакет - Турецкий',
+    'SubHeaderModuleTurkishLanguagePack' => 'Комплексная поддержка турецкого языка для системы MikoPBX',
+];
